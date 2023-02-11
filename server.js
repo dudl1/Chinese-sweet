f0a6262427ec65ca0bcd950ae6dc9d0e656f5f5e
dudl1 <80582147+dudl1@users.noreply.github.com>
2023-02-11 20:34:03 +0700

    Add full file

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const {createClient} = require("@supabase/supabase-js");
+const nodemailer = require("nodemailer");
+const app = express();
+
+const port = 3000;
+
+const supabase = createClient(
+  "https://qtsdplzasxqzgskoybdj.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0c2RwbHphc3hxemdza295YmRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzYwNDYwMzUsImV4cCI6MTk5MTYyMjAzNX0.hvCWqLQqfHswrUkCKF6uvj3MKV6mBjHb_Iy9W8pMnEI"
+);
+
+async function getDataDb()
+{
+  const { data, error } = await supabase.from("appeal").select();
+  console.log(data);
+}
+getDataDb();
+
+app.get('/', function (req, res)
+{
+  app.use(express.static(__dirname + "/assets"));
+  res.sendFile(__dirname + '/assets/index.html');
+})
+
+app.listen(port, function() {port;})
